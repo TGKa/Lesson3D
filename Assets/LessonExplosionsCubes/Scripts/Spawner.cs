076b2cbd0251c66fc7a3b3c75f84b569e11e5201
TGKa <eskov.ilia@yandex.ru>
2024-06-18 12:26:36 +0300

    Refactoring

--- Assets/LessonExplosionsCubes/Scripts/Spawner.cs
+++ Assets/LessonExplosionsCubes/Scripts/Spawner.cs
@@ -4,45 +4,48 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     private const int MinCountCube = 2;
-    private const int MaxCountCube = 7;
+    private const int MaxCountCube = 6;
 
     [SerializeField] private List<Cube> _cubes;
     [SerializeField] private Cube _prefab;
     [SerializeField] private Transform _container;
+    [SerializeField] private float _explosionForce;
+    [SerializeField] private float _explosionRadius;
 
     private Cube _currentCube;
 
     private void OnEnable()
     {
         foreach (var cube in _cubes)
-            cube.CubeClicked += OnCubeClicked;
+            cube.Clicked += OnClicked;
     }
 
     private void OnDisable()
     {
         foreach (var cube in _cubes)
-            cube.CubeClicked -= OnCubeClicked;
+            cube.Clicked -= OnClicked;
     }
 
-    private void OnCubeClicked(Cube cube, bool canSpawn)
+    private void OnClicked(Cube cube, bool canSpawn)
     {
         _currentCube = cube;
 
         if (canSpawn)
             Spawn(cube.transform.position);
 
-        cube.CubeClicked -= OnCubeClicked;
+        cube.Clicked -= OnClicked;
         _cubes.Remove(cube);
     }
 
     private void Spawn(Vector3 position)
     {
-        int countCubeSpawning = Random.Range(MinCountCube, MaxCountCube);
+        int countCubeSpawning = Random.Range(MinCountCube, MaxCountCube + 1);
         
         for (int i = 0; i < countCubeSpawning; i++)
         {
             Cube newCube = Instantiate(_prefab, position, Quaternion.identity,_container);
             InitializeCube(newCube);
+            ScatterCube(newCube,position);
         }
     }
 
@@ -52,7 +55,13 @@ public class Spawner : MonoBehaviour
         int chanceFalling = _currentCube.ChanceFalling / 2;
 
         cube.Init(scale, chanceFalling);
-        cube.CubeClicked += OnCubeClicked;
+        cube.Clicked += OnClicked;
         _cubes.Add(cube);
+    }
+
+    private void ScatterCube(Cube cube, Vector3 explosionPosition)
+    {
+        if (cube.TryGetComponent<Rigidbody>(out Rigidbody rigidbody))
+            rigidbody.AddExplosionForce(_explosionForce, explosionPosition, _explosionRadius);
     }
 }
